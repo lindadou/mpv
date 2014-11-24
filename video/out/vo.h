37b4285664d0431ace4ba1fed9e132fc39787a4c
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2014-11-24 21:26:04 +0100

    vo: move frame timing struct in it’s own header
    This is needed so that we can pass down the struct to gl_video in later
    commits, without coupling it to vo.h directly. gl_video still recursively
    includes vo.h but is mostly decoupled from it.

--- video/out/vo.h
+++ video/out/vo.h
@@ -26,6 +26,7 @@
 #include <inttypes.h>
 #include <stdbool.h>
 
+#include "video/frame_timing.h"
 #include "video/img_format.h"
 #include "common/common.h"
 #include "options/options.h"
@@ -170,12 +171,6 @@ struct vo_extra {
     struct mpv_opengl_cb_context *opengl_cb_context;
 };
 
-struct vo_image_timing {
-    int64_t pts;
-    int64_t prev_vsync;
-    int64_t next_vsync;
-};
-
 struct vo_driver {
     // Encoding functionality, which can be invoked via --o only.
     bool encode;
@@ -227,7 +222,7 @@ struct vo_driver {
      * information
      */
     void (*draw_image_timed)(struct vo *vo, struct mp_image *mpi,
-                             struct vo_image_timing *t);
+                             struct frame_timing *t);
 
     /*
      * Blit/Flip buffer to the screen. Must be called after each frame!
